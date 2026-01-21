d65390c0981bd6e1f6aaa50a17d01ad8ed71efdf
shuklaom <shuklaom2002@gmail.com>
2026-01-21 01:00:53 -0600

    Added all my skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -14,7 +14,16 @@ const Skills = () => {
     },
     {
       title: 'Certifications',
-      skills: ['Unreal Engine 5 C++ The Ultimate Game Developer', 'C++ Programming for Unreal Game Development', 'C++ Class Development', 'Microsoft Technology Associate: Introduction to Programming with Java', 'Apple Certified Repair Technician']
+      skills: ['Apple Certified Repair Technician', 
+              'Dell Certified Repair Technician', 
+              'C++ Class Development', 
+              'C++ Programming for Unreal Game Development', 
+              'Intermediate Object-Oriented Programming for Unreal Games', 
+              'Introduction to C++ Programming and Unreal', 
+              'More C++ Programming and Unreal',
+              'Unreal Engine 5 C++ — The Ultimate Game Developer',
+              'Microsoft Technology Associate – Introduction to Programming with Java',
+              'Rank #1 for C++ Programming on HackerRank']
     }
   ];
 
